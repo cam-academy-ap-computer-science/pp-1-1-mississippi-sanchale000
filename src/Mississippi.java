819e0fee4479bdfde724633643ba840f9a439d3a
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-13 07:42:48 -0700

    Working on M

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -2,8 +2,15 @@
 public class Mississippi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 }
+	public static void M{
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+	}
